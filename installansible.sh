44459559b05a86b2fee67c19e443eb14eca07030
Brandon Lee <22154488+brandonleegit@users.noreply.github.com>
2021-10-28 14:22:27 -0500

    Add files via upload

---
+++ installansible.sh
@@ -0,0 +1,8 @@
+sudo apt-get install gcc python-dev libkrb5-dev -y
+sudo apt-get install python3-pip -y
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade virtualenv
+pip3 install pywinrm
+sudo apt install krb5-user -y
+sudo apt-get install python3-kerberos -y
+sudo pip3 install ansible
